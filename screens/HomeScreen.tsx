b06903c99a9b4f8abd23e846390d1631b1c63228
bakarekeandrew <bakarekeandrew@gmail.com>
2024-07-03 22:43:02 +0100

    added broadcast receiver

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, Switch } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useColorScheme } from 'nativewind';
+import { StatusBar } from 'expo-status-bar';
+import useConnectivity from '../components/useConnectivity';
+
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
-}
+  // const batteryLevel = BatteryLevel();
+  const isConnected = useConnectivity();
 
-export default HomeScreen
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <SafeAreaView className={`flex-1 items-center justify-center ${colorScheme === 'dark' ? 'bg-neutral-900' : 'bg-white'}`}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <View className="flex-row">
+        <Text className={`pt-3 ${colorScheme === 'dark' ? 'text-white' : 'text-black'}`}>Theme</Text>
+        <Switch value={colorScheme === 'dark'} onValueChange={toggleColorScheme} />
+      </View>
+      <Text className={`${colorScheme === 'dark' ? 'text-white' : 'text-black'}`}>HomeScreen back</Text>
+      <Text className={`${colorScheme === 'dark' ? 'text-white' : 'text-black'}`}>
+        By following these steps, your app should be able to persist user sessions and keep users logged in even after they close and reopen the app.
+      </Text>
+      <Text className={`${colorScheme === 'dark' ? 'text-white' : 'text-black'}`}>
+        {isConnected ? 'You are online.' : 'You are offline.'}
+      </Text>
+
+    </SafeAreaView> 
+  );
+};
+
+export default HomeScreen;
